927b03c85eb65f086b97e1075563f2dd02c42797
022020 <natashia.virnilia@banksinarmas.com>
2025-11-19 14:21:07 +0700

    init

---
+++ src/main/java/com/natashiavirnilia/creditsimulator/CreditsimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.natashiavirnilia.creditsimulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditsimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditsimulatorApplication.class, args);
+	}
+
+}
