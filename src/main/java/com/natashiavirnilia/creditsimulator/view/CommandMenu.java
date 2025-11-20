9728a79c24002694b98519ebed4f0727877fa774
Natashia Virnilia <natavirni@gmail.com>
2025-11-20 20:52:38 +0700

    update menu

--- src/main/java/com/natashiavirnilia/creditsimulator/view/CommandMenu.java
+++ src/main/java/com/natashiavirnilia/creditsimulator/view/CommandMenu.java
@@ -4,7 +4,7 @@ public class CommandMenu {
     public static void showMenu() {
         System.out.println("=== COMMAND LIST ===");
         System.out.println("calculate   : Hitung cicilan baru");
-        System.out.println("load        : Load existing calculation dari file/API");
+        System.out.println("load        : Load existing calculation dari file");
         System.out.println("show        : Tampilkan semua perintah");
         System.out.println("save sheet  : Simpan hasil ke sheet baru");
         System.out.println("switch sheet: Pindah ke sheet lain");
